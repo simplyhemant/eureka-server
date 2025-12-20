4917086e9888ca326da04448a1fdc39251ac3d86
simplyhemant <hs2312hemant@gmail.com>
2025-12-21 01:52:13 +0530

    eureka server

---
+++ src/test/java/com/simply/eureka_server/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simply.eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
